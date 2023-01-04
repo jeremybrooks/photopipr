5bdfd777d0c58d87c613c68933a0da14be3845ec
jeremybrooks <jeremyb@whirljack.net>
2023-01-04 15:54:59 -0800

    Added comments

--- src/main/java/net/jeremybrooks/photopipr/PPConstants.java
+++ src/main/java/net/jeremybrooks/photopipr/PPConstants.java
@@ -22,22 +22,59 @@ package net.jeremybrooks.photopipr;
 import java.awt.Color;
 import java.util.List;
 
+/**
+ * Constants used throughout the PhotoPipr project.
+ */
 public class PPConstants {
   public enum TagMode {
     ALL, ANY
   }
+
+  /**
+   * Specify how photos are selected from a directory.
+   */
   public enum SelectionOrder {
+    /**
+     * Select photos randomly.
+     */
     RANDOM,
+    /**
+     * Select photos in date descending order (oldest first).
+     */
     DATE_DESC,
+    /**
+     * Select photos in date ascending order (newest first).
+     */
     DATE_ASC,
+
+    /**
+     * Select photos in alphabetic ascending order (A-Z).
+     */
     ALPHA_ASC,
+    /**
+     * Select photos in alphabetic descending order (Z-A).
+     */
     ALPHA_DESC
   }
 
+  /**
+   * Specify how a timer works.
+   */
   public enum TimerMode {
-    TIMER, ALARM
+    /**
+     * A timer waits for a period of time, for example 1 hour.
+     */
+    TIMER,
+
+    /**
+     * An alarm waits until a specific time, for example 6:00 AM.
+     */
+    ALARM
   }
 
+  /**
+   *
+   */
   public enum DirectoryCreateStrategy {
     NO_NEW_DIRECTORIES,
     DATE_TAKEN,
